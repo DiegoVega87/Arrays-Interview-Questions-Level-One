eddcf495ba672330efbe5923ef856ed412e186fa
Diego Vega <diegovega@me.com>
2023-07-12 17:22:20 +0200

    Add examples

--- src/ZeroMovement.java
+++ src/ZeroMovement.java
@@ -6,5 +6,15 @@ public class ZeroMovement {
     * We have noticed that our system is slowed down by the presence of zeros in our integer arrays.
     * We need a function that can move all the zeros to the end of the array without changing the order
     * of the other elements. Bonus: Can you achieve this without copying the list (in-place)?
+    *
+    * Example 1:
+    *   int[] nums = {0, 1, 0, 3, 12};
+    *   output: [1, 3, 12, 0, 0]
+    *
+    * Example 2:
+    *   int[] nums = {4, 0, 5, 0, 3, 0};
+    *   [4, 5, 3, 0, 0, 0]
+    *
+    *
     * */
 }
