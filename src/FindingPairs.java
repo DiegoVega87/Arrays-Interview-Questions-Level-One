b3aef4a766d499995718ad9b2621fd79d9257517
Diego Vega <diegovega@me.com>
2023-07-12 19:14:46 +0200

    Add problem explanation and examples

---
+++ src/FindingPairs.java
@@ -0,0 +1,21 @@
+public class FindingPairs {
+
+    /*
+    * We have a list of numbers representing the prices of our products. We want to create a feature
+    * that allows our users to search for pairs of products whose prices add up to a specific target amount.
+    * Write a function that takes in an array of integers representing the prices of our products and an integer target,
+    * and returns the indices of the two products whose prices add up to the target amount.
+    * Each product can only be used once and the output can be in any order.
+    *
+    * Example 1:
+    *   prices = {10, 20, 30, 40, 50};
+    *   target = 60;
+    *   expectedOutput = {0, 4};
+    *   Explanation: The prices at indices 0 and 4 add up to the target amount of 60.
+    *
+    * Example 2:
+    *   prices = {5, 15, 25, 35, 45};
+    *   target = 40;
+    *   expectedOutput = {1, 2};
+    * */
+}
