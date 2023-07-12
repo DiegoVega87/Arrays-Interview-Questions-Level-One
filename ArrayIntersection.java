da1b6230268c52d6f4c947018a88556b31355d38
Diego Vega <diegovega@me.com>
2023-07-12 13:04:08 +0200

    Add the problem statement

---
+++ ArrayIntersection.java
@@ -0,0 +1,13 @@
+public class ArrayIntersection {
+
+    /*
+    * Array Intersection for Sales Analytics
+    * As a sales analytics startup, we need to identify the common customers between two sales datasets.
+    * We have two integer arrays representing the customer IDs of each dataset.
+    * We need a function that returns an array of the customer IDs that appear in both datasets.
+    * The output should contain each customer ID as many times as it appears in both datasets and can be in any order.
+    * Can you help us write a function that solves this problem efficiently?
+    *
+    *
+    * */
+}
