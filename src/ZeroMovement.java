0973c966f33e8394d6cd0f83a6e8b200215b288d
Diego Vega <diegovega@me.com>
2023-07-12 17:17:16 +0200

    Add the problem requirements

---
+++ src/ZeroMovement.java
@@ -0,0 +1,10 @@
+public class ZeroMovement {
+
+    /*
+    * Zero Movement
+    * We are building a platform that requires us to process large amounts of data.
+    * We have noticed that our system is slowed down by the presence of zeros in our integer arrays.
+    * We need a function that can move all the zeros to the end of the array without changing the order
+    * of the other elements. Bonus: Can you achieve this without copying the list (in-place)?
+    * */
+}
