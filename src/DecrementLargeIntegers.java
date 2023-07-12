77a24d15e5a4ffa51d3d7bd7aa9fd4f718b76872
Diego Vega <diegovega@me.com>
2023-07-12 16:39:06 +0200

    Add problem statement

---
+++ src/DecrementLargeIntegers.java
@@ -0,0 +1,22 @@
+public class DecrementLargeIntegers {
+
+    /*
+    * Decrementing Large Integers
+    * Our startup focuses on optimizing operations for large integers represented as integer arrays.
+    * Currently, we're facing a challenge in implementing a function that efficiently decrements a
+    * given large integer by one. The expected outcome is an integer array containing the digits of
+    * the decremented value.
+    *
+    * Example 1:
+    *   int[] arr = {1, 2, 3, 4};
+    *   result should be {1, 2, 3, 3}
+    *
+    * Example 2:
+    *   int[] arr = {9, 9, 9, 9};
+    *   result should be {9, 9, 9, 8}
+    *
+    * Example 3:
+    *   int[] arr = {1, 0, 0, 0};
+    *   result should be {0, 9, 9, 9}
+    * */
+}
