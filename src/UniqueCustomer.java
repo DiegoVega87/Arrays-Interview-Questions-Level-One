ac0bbfb82af9e52b5b7e7dc994a74a7ad29fab17
Diego Vega <diegovega@me.com>
2023-07-12 12:56:51 +0200

    Add the Solution

--- src/UniqueCustomer.java
+++ src/UniqueCustomer.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class UniqueCustomer {
 
     /*
@@ -15,5 +18,27 @@ public class UniqueCustomer {
     *   customerIDs = {10, 20, 30, 40, 50, 60, 10, 20, 30, 40, 50};
     *   Output: 60
     *
+    * Logic: Traverse the list and keep the frequency of each customer ID along with each customer ID.
+    *   Traverse the list with the IDs (as keys) and frequencies (as values) and return the key where
+    *   the frequency is 1.
+    *
     * */
+
+    public static int findUniqueCustomerId(int[] customerIDs){
+
+        Map<Integer,Integer> IDs = new HashMap<>();
+
+        for(int id : customerIDs){
+            int frequency = IDs.getOrDefault(id, 0);
+            IDs.put(id, frequency + 1);
+        }
+
+        for(Integer key : IDs.keySet()){
+           if(IDs.get(key) == 1){
+               return key;
+           }
+        }
+
+        return -1; // There is no unique ID.
+    }
 }
