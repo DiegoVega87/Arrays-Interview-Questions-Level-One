626f8be83514e963cbe5ad8e5af8db3adeaf21f0
Diego Vega <diegovega@me.com>
2023-07-12 12:45:49 +0200

    Add the problem statement

---
+++ src/UniqueCustomer.java
@@ -0,0 +1,19 @@
+public class UniqueCustomer {
+
+    /*
+    * Identify Unique Customer
+    * You have a list of customer IDs in an array called ""customerIDs"".
+    * Each customer ID appears twice except for one unique customer.
+    *  Write a function to identify the unique customer ID.
+    * Can you identify a solution that only needs linear run time?
+    *
+    * Example 1:
+    *   customerIDs = {1, 2, 3, 4, 5, 1, 2, 3, 4};
+    *   Output: 5
+    *
+    * Example 2:
+    *   customerIDs = {10, 20, 30, 40, 50, 60, 10, 20, 30, 40, 50};
+    *   Output: 60
+    *
+    * */
+}
