7d9307db47edcfd9bcb27eae7e053325a501f4e3
Diego Vega <diegovega@me.com>
2023-07-12 12:11:46 +0200

    Add the problem statement and examples

---
+++ src/DuplicateDetection.java
@@ -0,0 +1,24 @@
+public class DuplicateDetection {
+
+    /*
+    * You are building a platform that allows users to upload and share their favorite products.
+    * To ensure the quality of the platform, you want to detect if any product is uploaded more than once.
+    * You have an array of integers representing the product IDs uploaded by users.
+    * Write a function that takes in an integer array and returns true if any value appears at least twice in the array,
+    * and false if every element is distinct.
+    *
+    * Example 1:
+    *   [] arr = {1, 2, 3, 4, 5};
+    *   result = false
+    *
+    * Example 2:
+    *   arr = {1, 2, 3, 4, 5, 1};
+    *   result = true
+    *
+    *
+     * */
+    public static boolean hasDuplicate(int[] arr){
+
+
+    }
+}
