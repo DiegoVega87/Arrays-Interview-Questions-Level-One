913b3ed856dd8e4973169f462d8d321410b67bac
Diego Vega <diegovega@me.com>
2023-07-13 10:51:33 +0200

    Add problem explanations

---
+++ src/ValidateSudoku.java
@@ -0,0 +1,39 @@
+public class ValidateSudoku {
+
+    /*
+    * As a startup that provides a platform for playing Sudoku, we need to ensure that the Sudoku
+    * boards created by our users are valid. We want you to implement a function that takes in a
+    * Sudoku board as a list of lists and returns a boolean indicating whether the board is valid or not.
+    * The board is considered valid if each row, column, and 3x3 sub-box contains the digits 1-9 without repetition.
+    * Empty cells can be ignored.
+    *
+    * Example 1:
+    *   board = {
+    *   {5, 3, 0, 0, 7, 0, 0, 0, 0},
+    *   {6, 0, 0, 1, 9, 5, 0, 0, 0},
+    *   {0, 9, 8, 0, 0, 0, 0, 6, 0},
+    *   {8, 0, 0, 0, 6, 0, 0, 0, 3},
+    *   {4, 0, 0, 8, 0, 3, 0, 0, 1},
+    *   {7, 0, 0, 0, 2, 0, 0, 0, 6},
+    *   {0, 6, 0, 0, 0, 0, 2, 8, 0},
+    *   {0, 0, 0, 4, 1, 9, 0, 0, 5},
+    *   {0, 0, 0, 0, 8, 0, 0, 7, 8}}
+    *
+    *   expectedOutput = true;
+    *
+    * Example 2:
+    *   board = {
+    *   {5, 3, 0, 0, 7, 0, 0, 0, 0},
+    *   {6, 0, 0, 1, 9, 5, 0, 0, 0},
+    *   {0, 9, 8, 0, 0, 0, 0, 6, 0},
+    *   {8, 0, 0, 0, 6, 0, 0, 0, 3},
+    *   {4, 0, 0, 8, 0, 3, 0, 0, 1},
+    *   {7, 0, 0, 0, 2, 0, 0, 0, 6},
+    *   {0, 6, 0, 0, 0, 0, 2, 8, 0},
+    *   {0, 0, 0, 4, 1, 9, 0, 0, 5},
+    *   {0, 0, 0, 0, 8, 0, 0, 7, 8}}
+    *
+    *   expectedOutput = false;
+    *
+    * */
+}
