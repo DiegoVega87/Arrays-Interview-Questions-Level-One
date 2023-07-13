a4946fbca7839c58e4283b24fc25c155221f230b
Diego Vega <diegovega@me.com>
2023-07-13 12:07:54 +0200

    Add problem explanation

---
+++ src/FlipImage.java
@@ -0,0 +1,15 @@
+public class FlipImage {
+
+    /*
+    * Our startup is developing a photo editing app, and we need to implement a feature that allows users
+    * to rotate their (squared) images by 180 degrees. The output should be the same 2D matrix with the rotated image.
+    *
+    * Example 1:
+    *   image = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    *   expectedOutput = {{9, 8, 7}, {6, 5, 4}, {3, 2, 1}};
+    *
+    * Example 2:
+    *   image = {{1, 2}, {3, 4}};
+    *   expectedOutput = {{4, 3}, {2, 1}};
+     * */
+}
